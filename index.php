c77e073a8b901c805dd692414f26167f10f3e2d6
Avinash <avinas9624@gmail.com>
2019-10-24 18:16:50 -0700

    theme and templates dekh lo

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-echo "Welcome to We Media Program";
-
+echo "Hello ";
 ?>
-<br>
-<a href="Super_Admin/index.php">Super Admin</a><br>
-<a href="Sub_Admin/index.php">Sub Admin</a><br>
-<a href="Operator/index.php">Operator</a><br>
