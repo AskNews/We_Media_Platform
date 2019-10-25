4709f6f846d0c0b0eb9bcebc77124c95c8b34780
shabnam1999 <shabnamsiddique20@gmail.com>
2019-10-25 13:46:44 +0530

    Shabbu ne kiya

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-echo "Hello ";
-?>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+    <a href="Content_Creator/index.php">Content Creator</a>
+    <a href="Ad_Creator/index.php">Ad Creator</a>
+</body>
+</html>
