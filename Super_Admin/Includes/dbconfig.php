2e410d00a7143c4c88eae7e7ccdfb6beefb98710
AvinashKishan <avinas9624@gmail.com>
2019-10-31 03:38:20 -0700

    mene new desktop pese kiya

--- Super_Admin/Includes/dbconfig.php
+++ Super_Admin/Includes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 //local and remote server connection
-if($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '::1'){
+if($_SERVER['REMOTE_ADDR'] == '127.0.0.1:3307' && $_SERVER['REMOTE_ADDR'] == '::1'){
 	
 	@define("hostname","127.0.0.1:3307");
 	@define("username","root");
@@ -8,10 +8,11 @@ if($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '::1'){
 	@define("database","dbasknews");
 	
 	}else{-
-		define("hostname","localhost");
-	define("username","unknown");
-	define("password","unknown");
-	define("database","unknown");
+		@define("hostname","127.0.0.1:3306");
+	@define("username","root");
+	@define("password","");
+	@define("database","dbasknews");
+	
 		}
 
 		// Create connection
