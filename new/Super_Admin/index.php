abdae4e6fcbc596d1bc1b69615df8483cab5f07e
Avinash <avinas9624@gmail.com>
2019-11-28 21:23:45 -0800

    dash

--- new/Super_Admin/index.php
+++ new/Super_Admin/index.php
@@ -5,35 +5,184 @@ include "includes/header.php";
 		<!-- MAIN -->
 		
     <div class="w3-container w3-card">
-    <h3>Dashboard</h3>
-<br>
-<div class="w3-card w3-quarter">
-  <p>w3-card</p>
-</div> 
-<div class="w3-card w3-quarter">
-  <p>w3-card</p>
+    <h3>Dashboard <i class="fa fa-tachometer"></i></h3>
+    <br>
+<table class="w3-responsive">
+<tr><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total News &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >10.4K</font> 
+  </div>
+  <br>
 </div>
-<div class="w3-card w3-quarter">
-  <p>w3-card</p>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Operators &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >30</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Ads &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400</font> 
+  </div>
+  <br>
 </div> 
-<div class="w3-card w3-quarter">
-  <p>w3-card</p>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Sub Admin &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >16</font> 
+  </div>
+  <br>
 </div> 
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Sub Admin &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >16</font> 
+  </div>
+  <br>
+</div> 
+</td></tr></table>
+<br>
+<table class="w3-responsive">
+<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Views &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >10.4K</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total viewers &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >30</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Content Creator &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400</font> 
+  </div>
+  <br>
+</div> 
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Ad Creator &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >16</font> 
+  </div>
+  <br>
+</div> 
+</td></tr></table>
+
 <br><br>&nbsp;
 </div><br><br>
 <div class="w3-container w3-card">
+    <h3>Dashboard</h3>
 <br>
-<div class="w3-card w3-third">
-  <p>w3-card</p>
-</div> 
-<div class="w3-card w3-third">
-  <p>w3-card</p>
+<div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;System Logs &nbsp; <i class="fa fa-history"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400K</font> 
+  </div>
+  <br>
 </div>
-<div class="w3-card w3-third">
-  <p>w3-card</p>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Module Logs &nbsp; <i class="fa fa-history"></i></p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >400</font> 
+  </div>
+  <br>
+</div>
+
+<div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Server Details&nbsp; <i class="fa fa-history"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400</font> 
+  </div>
+  <br>
+</div> 
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Log Report&nbsp; <i class="fa fa-user"></i></p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >400</font> 
+  </div>
+  <br>
 </div> 
 <br><br>&nbsp;
+</div><br><br>
+
+<div class="w3-container w3-card">
+<h3>Sub Admin Section <i class="fa fa-users"></i></h3>
+<br>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Active Sub Admin</p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >10</font> 
+  </div>
+  <br></div> 
+<div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;In-Active Sub Admin</p>
+  <div class="w3-center w3-pink">
+  <font size="6" >6</font> 
+  </div>
+  <br></div>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Sub Admin Messages</p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >166</font> 
+  </div><br>
+ </div> 
+ <div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Sub Admin Reports</p>
+  <div class="w3-center w3-pink">
+  <font size="6" >45</font> 
+  </div><br>
+ </div> &nbsp;
+ <br><br>
 </div>
+<br><br>
+
+<div class="w3-container w3-card">
+<h3>Operator Section <i class="fa fa-users"></i></h3>
+<br>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Active Operators</p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >106</font> 
+  </div>
+  <br></div> 
+<div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;In-Active Operators</p>
+  <div class="w3-center w3-pink">
+  <font size="6" >12</font> 
+  </div>
+  <br></div>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Operators Messages</p>
+  <div class="w3-center w3-yellow">
+  <font size="6" >1.4K</font> 
+  </div><br>
+ </div> 
+ <div class="w3-card w3-quarter w3-hover-shadow">
+<p>&nbsp;&nbsp;Operators Reports</p>
+  <div class="w3-center w3-pink">
+  <font size="6" >455</font> 
+  </div>&nbsp;
+ </div> 
+
+<br><br>&nbsp;
+</div><br>
 
 
 		<!-- END MAIN -->
--- new/sub_admin/index.php
+++ new/sub_admin/index.php
@@ -3,36 +3,74 @@ $type="index";
 include "includes/header.php";
 ?>
 		<!-- MAIN -->
-		
-<div class="w3-responsive w3-card-4">
-<table class="w3-table w3-striped w3-bordered">
-<thead>
-<tr class="w3-theme">
-  <th>First Name</th>
-  <th>Last Name</th>
-  <th>Points</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-  <td>Jill</td>
-  <td>Smith</td>
-  <td>50</td>
-</tr>
-<tr class="w3-white">
-  <td>Eve</td>
-  <td>Jackson</td>
-  <td>94</td>
-</tr>
-<tr>
-  <td>Adam</td>
-  <td>Johnson</td>
-  <td>67</td>
-</tr>
-</tbody>
-</table>
+	
+    <div class="w3-container w3-card">
+    <h3>Dashboard <i class="fa fa-tachometer"></i></h3>
+    <br>
+<table class="w3-responsive">
+<tr><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Categories &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >10.4K</font> 
+  </div>
+  <br>
 </div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Gallery &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >30</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Pictures &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400</font> 
+  </div>
+  <br>
+</div> 
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Sub Admin &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >16</font> 
+  </div>
+  <br>
+</div> 
+</td></tr></table>
+<br>
+<table class="w3-responsive">
+<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Views &nbsp;<i class="fa fa-newspaper-o"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >10.4K</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-border w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total viewers &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-green">
+  <font size="6" >30</font> 
+  </div>
+  <br>
+</div>
+</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td>
+<div class="w3-card w3-quarter w3-hover-shadow" style="width: 200px;">
+<p>&nbsp;&nbsp;Total Content Creator &nbsp;<i class="fa fa-users"></i></p>
+  <div class="w3-center w3-blue">
+  <font size="6" >400</font> 
+  </div>
+  <br>
+</div> 
+</td></tr></table>
 
+<br><br>&nbsp;
+</div><br><br>
 
 		<!-- END MAIN -->
         <?php
