4b8cc7213a6768974df5587d99a17ef73b5fb26f
shabnam1999 <shabnamsiddique20@gmail.com>
2020-04-27 09:29:03 -0700

    graph done

--- Ad_Creator/index.php
+++ Ad_Creator/index.php
@@ -61,7 +61,57 @@ $ttl_pen_ad=$ttl_pen_ad['ttl_pen_ad'];
             </div>
         </div>
     </div>
-</section>                          
+</section>      
+<section>
+    <div class="section__content section__content--p100">
+        <div class="container-fluid">
+        <div class="row">
+        <div class="col-xl-8">
+                                <div class="recent-report2">
+                                    <h3 class="title-3">Month wise Ads</h3>
+                                    <div class="recent-report__chart">
+                                    <div id="ad_graph"></div>
+                                    <?php 
+                        $chart_qry=mysqli_query($con,"select Month,count(month(publish_date)) AdsCountMonth FROM
+                        ( select *,( CASE 
+                            WHEN month(publish_date)=01 THEN 'January'
+                            WHEN month(publish_date)=02 THEN 'February'
+                            WHEN month(publish_date)=03 THEN 'March'
+                            WHEN month(publish_date)=04 THEN 'April' 
+                            WHEN month(publish_date)=05 THEN 'May'
+                            WHEN month(publish_date)=06 THEN 'June' 
+                            WHEN month(publish_date)=07 THEN 'July' 
+                            WHEN month(publish_date)=08 THEN 'August' 
+                            WHEN month(publish_date)=09 THEN 'September' 
+                            WHEN month(publish_date)=10 THEN 'Octuber'
+                            WHEN month(publish_date)=11 THEN 'November' 
+                            WHEN month(publish_date)=12 THEN 'December' 
+                            END) AS Month from tbl_adunit ) AS Result where approve=1 and ad_creator_id=".$creatorid." GROUP BY Month");
+                        $arr=array();
+                        while($row=mysqli_fetch_array($chart_qry))
+                        {
+                            $arr[]=$row;
+                        }
+                        ?>
+                        <script src='js/jquery.min.js'></script>
+                        <script src='js/morris.min.js'></script>
+                        <script src='js/raphael-min.js'></script>
+                        <script type="text/javascript" language="javascript" >
+                        Morris.Bar({
+                            element:'ad_graph',
+                            data:<?php echo json_encode($arr); ?>,
+                            xkey:'Month',
+                            ykeys:['AdsCountMonth'],
+                            labels:['Ads Count']
+                        });
+                        </script>                            
+                                    </div>
+                                </div>
+        </div>
+        </div>
+    </div>
+</section>
+                                
 </div>
 </div>
 <?php
